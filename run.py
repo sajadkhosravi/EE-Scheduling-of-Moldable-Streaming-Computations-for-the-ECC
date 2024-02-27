0fe22b53410bfd56827000ef3a37142589deb4f7
sajadkhosravi <sajadkhosravi1996@gmail.com>
2024-02-27 16:04:30 +0100

    Add Random Task Graph Generator

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import subprocess
 
 for i in range(10, 100):
-  subprocess.run("mkdir .\\output\\Base\\Random" + str(i), shell=True, check=True)
-  command_template = "python ./src/main.py Base ./data/Workflows/RandomChain/10_tasks_12_task_sets_random_graph_"+str(i)+".graphml .\\output\\Base\\Random" + str(i)
-  try:
-    # Execute the command using subprocess
-    subprocess.run(command_template, shell=True, check=True)
-  except subprocess.CalledProcessError as e:
-    print(f"Error running command: {e}")
+    subprocess.run("mkdir .\\output\\Base\\Random" + str(i), shell=True, check=True)
+    command_template = "python ./src/main.py Base ./data/Workflows/RandomChain/10_tasks_12_task_sets_random_graph_" + str(
+        i) + ".graphml .\\output\\Base\\Random" + str(i)
+    try:
+        # Execute the command using subprocess
+        subprocess.run(command_template, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running command: {e}")
 
 print("Commands executed successfully.")
