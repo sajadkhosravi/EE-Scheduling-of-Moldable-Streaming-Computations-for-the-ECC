85537c5129f080e7ffc6c9b0fdc1fad2ef4c0293
sajadkhosravi <sajadkhosravi1996@gmail.com>
2024-03-03 15:09:45 +0100

    Add heuristic approach Add environment size variable

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ from optimizer.ILP.baseOptimizer import BaseOptimizer
 from optimizer.ILP.relaxedOptimizer import RelaxedOptimizer
 from optimizer.ILP.symmetryBaseOptimizer import SymmetryBaseOptimizer
 from optimizer.ILP.symmetryRelaxedOptimizer import SymmetryRelaxedOptimizer
-from optimizer.heuristic.slackBasedHeuristic import SlackBasedHeuristic
+from optimizer.heuristic.baseHeuristic import Heuristic
+from optimizer.heuristic.relaxedHeuristic import RelaxedHeuristic
 from data.Environments.default import env as default_env
+from data.Environments.default_small import env as default_small_env
 from data.Environments.relaxed import env as relaxed_env
 from data.Environments.symmetry_default import env as symmetry_base_env
 from data.Environments.symmetry_relaxed import env as symmetry_relaxed_env
@@ -15,13 +17,18 @@ import igraph
 import time
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print("Please specify optimizer type, task set file (including path), and path to results directory!")
+    if len(sys.argv) < 5:
+        print("Please specify optimizer type, env size, task set file (including path), and path to results directory!")
         sys.exit(1)
 
     optimizer_type = sys.argv[1]
-    input_filename = sys.argv[2]
-    output_path = sys.argv[3]
+    env_size = sys.argv[2]
+    input_filename = sys.argv[3]
+    output_path = sys.argv[4]
+
+    base_env = default_env
+    if env_size == "small":
+        base_env = default_small_env
 
     task_graph = igraph.load(input_filename)
 
@@ -41,20 +48,22 @@ if __name__ == '__main__':
                                              symmetry_relaxed_env["execution_time_multipliers"],
                                              symmetry_relaxed_env["power"], symmetry_relaxed_env["level"])
 
-    elif optimizer_type == "SlackHeuristic":
-        optimizer = SlackBasedHeuristic(task_graph, relaxed_env["compute_resources"],
-                                             relaxed_env["network"],
-                                             relaxed_env["execution_time_multipliers"],
-                                             relaxed_env["power"])
+    elif optimizer_type == "BaseHeuristic":
+        optimizer = Heuristic(task_graph, base_env["compute_resources"],
+                                             base_env["network"],
+                                             base_env["execution_time_multipliers"],
+                                             base_env["power"])
+    elif optimizer_type == "RelaxedHeuristic":
+        optimizer = Heuristic(task_graph, relaxed_env["compute_resources"], relaxed_env["network"], relaxed_env["execution_time_multipliers"], relaxed_env["power"])
 
     else:
-        optimizer = BaseOptimizer(task_graph, default_env["compute_resources"], default_env["network"],
-                                  default_env["execution_time_multipliers"], default_env["power"])
+        # optimizer = BaseOptimizer(task_graph, default_env["compute_resources"], default_env["network"], default_env["execution_time_multipliers"], default_env["power"])
+        optimizer = BaseOptimizer(task_graph, base_env["compute_resources"], base_env["network"], base_env["execution_time_multipliers"], base_env["power"])
 
     optimizer.compute_deadline()
 
     start_time = time.process_time()
-    optimizer.optimize()
+    status = optimizer.optimize()
     end_time = time.process_time()
 
     print("Optimization (" + optimizer_type + ") Time: " + str(end_time - start_time))
@@ -63,8 +72,8 @@ if __name__ == '__main__':
     time_df = pd.DataFrame(time_dict)
 
     time_df.to_csv(output_path + "/time.csv")
-
-    optimizer.generate_result(output_path)
+    if status != -1:
+        optimizer.generate_result(output_path)
 
     if optimizer_type == "SymmetryBase" or optimizer_type == "SymmetryRelaxed":
         optimizer.data_completer(output_path)
